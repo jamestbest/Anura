167d5f974e95ae937c94ecbb651a0a2f785a85b4
james <james2004.coward@gmail.com>
2025-09-29 00:12:23 +0100

    Added SR Commons header

---
+++ Commons.h
@@ -0,0 +1,16 @@
+//
+// Created by jamescoward on 07/06/2024.
+//
+
+#ifndef COMMONS_H
+#define COMMONS_H
+
+#include <stdbool.h>
+#include <assert.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <stdarg.h>
+
+typedef unsigned int uint;
+
+#endif //COMMONS_H
