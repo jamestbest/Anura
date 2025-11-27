d3f98c6f761523e32102c9b1316ed8b4850e9111
james <james2004.coward@gmail.com>
2025-11-27 22:30:40 +0000

    Added target functions

--- Target.h
+++ Target.h
@@ -19,7 +19,9 @@ typedef struct Target {
     PROCESS_ID pid;
 
     long long (*target_place_bp_at_line)(uint32_t line);
-    long long (*target_place_bp_at_addr)(void* addr);
+    long long (*target_place_bp_at_addr)(void* addr, uint32_t line);
+
+    void (*target_breakpoint_hit_cleanup)();
 
     LineAddrRes (*target_get_addr_of_line)(uint32_t line);
 
@@ -28,6 +30,8 @@ typedef struct Target {
     PROCESS_ID (*target_launch_process)(const char* path, uint32_t argc, const char* argv[]);
     long long (*target_attach_process)(PROCESS_ID pid);
     int (*target_decode_file)(const char* filepath);
+
+    long long (*target_single_step_assembly)();
 } Target;
 
 typedef enum TARGETS {
