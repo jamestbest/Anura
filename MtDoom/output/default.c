5951c8ef586717cf865f759c3276b288ac286dcf
james <james2004.coward@gmail.com>
2026-02-12 22:06:59 +0000

    Updated default file with functions for disassembly

--- MtDoom/output/default.c
+++ MtDoom/output/default.c
@@ -1,12 +1,74 @@
 //
-// Created by jamestbest on 1/24/26.
+// Created by James Coward on 1/24/26.
 //
 
 #include "default.h"
 
+#include <stdlib.h>
+
 #include "Errors.h"
 
-ByteStream stream;
+ByteStream top_stream;
+
+const AVAL AVAL_EMPTY= (AVAL) {
+    .chosen_val= "",
+    .chosen_idx= AVAL_STATUS_SELECTED,
+    .parsed_successfully= true
+};
+
+ByteStream stream_construct(size_t size) {
+    ByteStream stream;
+    stream.raw_stream= malloc(size * sizeof(uint8_t));
+    stream.size= size * sizeof(uint8_t) * 8;
+    stream.max_pointer= 0;
+    stream.pointer= 0;
+    return stream;
+}
+
+ByteStream stream_create() {
+    return stream_construct(5);
+}
+
+void stream_destroy(ByteStream* stream) {
+    free(stream->raw_stream);
+    stream->size= 0;
+    stream->max_pointer= 0;
+}
+
+void stream_resize(ByteStream* stream) {
+    const size_t new_size= stream->size << 1;
+    void* new_stream= realloc(stream->raw_stream, new_size);
+    if (!new_stream) assert(false);
+
+    stream->size= new_size;
+    stream->raw_stream= new_stream;
+}
+
+void stream_add_bit(ByteStream* stream, bool value) {
+    if (stream->max_pointer == stream->size) stream_resize(stream);
+
+    const size_t idx= stream->max_pointer >> 3;
+    const size_t bit= 7 - (stream->max_pointer & 7);
+    stream->raw_stream[idx] &= ~(1 << bit);
+    stream->raw_stream[idx] |= value << bit;
+    stream->max_pointer++;
+}
+
+void stream_add(ByteStream* stream, uint64_t value, uint16_t bits) {
+    for (int i = bits - 1; i >= 0; i--) {
+        const bool bit= value >> i & 1;
+        stream_add_bit(stream, bit);
+    }
+}
+
+ByteStream stream_from_bytes(const uint8_t* bytes, const size_t byte_count) {
+    ByteStream stream= stream_construct(byte_count << 3);
+    for (size_t i = 0; i < byte_count; i++) {
+        stream.raw_stream[i]= bytes[i];
+    }
+    stream.max_pointer= byte_count << 3;
+    return stream;
+}
 
 bool bit_from_raw_stream(const uint8_t* stream, const uint64_t idx) {
     return ((stream[idx / 8]) >> (7 - (idx % 8)) & 1);
@@ -36,13 +98,6 @@ int consume_lit_binary(
     return SUCCESS;
 }
 
-ByteStream init_stream(uint8_t* raw_stream) {
-    return (ByteStream) {
-        .raw_stream= raw_stream,
-        .pointer= 0
-    };
-}
-
 // [[todo]] add the error checking from bit_from_stream
 bool read_bit(ByteStream* stream) {
     return bit_from_raw_stream(stream->raw_stream, stream->pointer++);
@@ -57,12 +112,16 @@ uint64_t read_bits(ByteStream* stream, uint32_t bits) {
     return res;
 }
 
-bool expect_bits(const uint8_t bits, const uint64_t bit_pattern) {
+bool expect_bits(const uint8_t bits, const uint64_t bit_pattern, ByteStream* stream) {
+    const size_t old_pointer= stream->pointer;
     for (int i = 0; i < bits; ++i) {
-        const bool dbit= read_bit(&stream);
-        const bool pbit= (bit_pattern >> i) & 1;
+        const bool dbit= read_bit(stream);
+        const bool pbit= (bit_pattern >> (bits - i - 1)) & 1;
 
-        if (dbit != pbit) return false;
+        if (dbit != pbit) {
+            stream->pointer= old_pointer;
+            return false;
+        }
     }
 
     return true;
@@ -72,7 +131,7 @@ char* evaluate_string(char* string, ...) {
     va_list args;
     va_start(args, string);
 
-    Buffer buffer;
+    Buffer buffer= buffer_create(BUFF_MIN);
 
     size_t idx= 0;
     size_t start= 0;
@@ -80,9 +139,7 @@ char* evaluate_string(char* string, ...) {
         const char c= string[idx];
 
         if (c == '{') {
-            string[idx]= '\0';
-            buffer_concat(&buffer, &string[start]);
-            string[idx]= '{';
+            buffer_nconcat(&buffer, &string[start], idx - start);
             while (string[idx] != '}') idx++;
             idx++;
             start= idx;
@@ -116,4 +173,34 @@ AVAL to_aval(char* data) {
         .chosen_val= data,
         .parsed_successfully= true
     };
+}
+
+AVAL data_to_aval(uint64_t value) {
+    return to_aval(data_to_string(value));
+}
+
+#define HEX_DIGITS(num) log2(num) / 4
+#define HEX_EXTRA_SIZE sizeof("-0x")
+#define MAX_HEX_CHARS HEX_DIGITS(UINT64_MAX) + HEX_EXTRA_SIZE
+
+char* data_to_string(int64_t value) {
+    char* buff= malloc(MAX_HEX_CHARS);
+    if (value < 0) {
+        snprintf(buff, MAX_HEX_CHARS, "-%#lx", value);
+    } else {
+        snprintf(buff, MAX_HEX_CHARS, "%#lx", value);
+    }
+    return buff;
+}
+
+void clear_aval(AVAL* aval) {
+    aval->parsed_successfully= false;
+    aval->chosen_idx= AVAL_STATUS_NONE;
+    aval->chosen_val= NULL;
+
+    for (size_t i = 0; i < aval->choices.pos; i++) {
+        const char* str= vector_get_unsafe(&aval->choices, i);
+        free((void*)str);
+    }
+    aval->choices.pos= 0;
 }
--- MtDoom/output/default.h
+++ MtDoom/output/default.h
@@ -1,5 +1,5 @@
 //
-// Created by jamestbest on 1/24/26.
+// Created by James Coward on 1/24/26.
 //
 
 #ifndef ANURA_DEFAULT_H
@@ -7,6 +7,7 @@
 
 #include <stdbool.h>
 #include <stdint.h>
+#include "math.h"
 
 #include "shared/Vector.h"
 #include "shared/Buffer.h"
@@ -17,11 +18,11 @@ typedef struct ByteStream {
     uint8_t* raw_stream;
     uint64_t pointer;
     uint64_t max_pointer;
+    uint64_t size;
 } ByteStream;
 
-extern ByteStream stream;
+extern ByteStream top_stream;
 
-ByteStream init_stream(uint8_t* raw_stream);
 bool read_bit(ByteStream* stream);
 
 typedef enum AVAL_STATUS {
@@ -36,11 +37,7 @@ typedef struct AVAL {
     bool parsed_successfully;
 } AVAL;
 
-const AVAL AVAL_EMPTY= (AVAL) {
-    .chosen_val= "",
-    .chosen_idx= AVAL_STATUS_SELECTED,
-    .parsed_successfully= true
-};
+extern const AVAL AVAL_EMPTY;
 
 typedef struct ParseRet {
     AVAL aval;
@@ -64,9 +61,10 @@ typedef struct StrSlice {
     const char first= str[0], second= str[1];                                                           \
     _Static_assert(first == '0' && (second == 'x' || second == 'X' || second == 'b' || second == 'B')); \
 }
-#define EXPECT_BITS(num) expect_bits(COUNT_ALT_BASE_DIGITS(num), num)
+#define EXPECT_BITS(num, stream) expect_bits(COUNT_ALT_BASE_DIGITS(num), num, stream)
+#define EXPECT_BYTE(num, stream) expect_bits(8, num, stream)
 
-bool expect_bits(uint8_t bits, uint64_t bit_pattern);
+bool expect_bits(uint8_t bits, uint64_t bit_pattern, ByteStream* stream);
 int init();
 
 uint64_t read_bits(ByteStream* stream, uint32_t bits);
@@ -74,6 +72,18 @@ uint64_t read_bits(ByteStream* stream, uint32_t bits);
 char* evaluate_string(char* string, ...);
 void set_aval(AVAL* dst, char* data);
 AVAL to_aval(char* data);
+AVAL data_to_aval(uint64_t value);
+char* data_to_string(int64_t value);
+void clear_aval(AVAL* aval);
+
+void stream_add_bit(ByteStream* stream, bool value);
+void stream_add(ByteStream* stream, uint64_t value, uint16_t bits);
+
+ByteStream stream_construct(size_t size);
+ByteStream stream_create();
+void stream_destroy(ByteStream* stream);
+
+ByteStream stream_from_bytes(const uint8_t* bytes, size_t byte_count);void reset();
 
 #define PARSE_SUCC(aval_) (ParseRet){.aval=aval_, .success= true}
 #define PARSE_SUCC_HIDDEN (ParseRet){.aval=AVAL_EMPTY, .success= true}
