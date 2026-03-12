61454be1d0edbde713fa7f8c9eefe5968636e2fc
James C <psyjc25@nottingham.ac.uk>
2026-03-12 19:43:22 +0000

    Added goto for gui

--- Palantir/Palantir.c
+++ Palantir/Palantir.c
@@ -11,14 +11,28 @@ static bool set_buffer_as_file(const char* file, GtkTextBuffer* buff) {
     return false;
 }
 
+void goto_line();
+
+static gboolean event_key_pressed_cb(
+    GtkWidget             *drawing_area,
+    guint                  keyval,
+    guint                  keycode,
+    GdkModifierType        state,
+    GtkEventControllerKey *event_controller
+) {
+    printf("Got event\n");
+    goto_line();
+}
+
+GtkTextBuffer *buffer;
+GtkWidget *view;
+
 static void
 activate (GtkApplication *app, gpointer user_data)
 {
     GtkWidget *window;
     GtkWidget *box;
-    GtkWidget *view;
     GtkWidget *scrolled;
-    GtkTextBuffer *buffer;
     GtkTextIter start, end;
     GtkTextTag *tag;
     GtkCssProvider *provider;
@@ -26,7 +40,7 @@ activate (GtkApplication *app, gpointer user_data)
 
     window = gtk_application_window_new(app);
     gtk_window_set_title(GTK_WINDOW(window), "Window");
-    gtk_window_set_default_size(GTK_WINDOW(window), 400, 300);
+    gtk_window_set_default_size(GTK_WINDOW(window), 1920, 1080);
 
     box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 6);
     gtk_window_set_child(GTK_WINDOW(window), box);
@@ -34,10 +48,12 @@ activate (GtkApplication *app, gpointer user_data)
     view = gtk_text_view_new();
     gtk_widget_set_hexpand(view, TRUE);
     gtk_widget_set_vexpand(view, TRUE);
+    gtk_text_view_set_editable(GTK_TEXT_VIEW(view), false);
 
     buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(view));
     // gtk_text_buffer_set_text(buffer, "Hello, this is some text", -1);
-    set_buffer_as_file("/home/jamestbest/test.s", buffer);
+    set_buffer_as_file("/home/james/UoNDocs/ATOMIC/Compiler.c", buffer);
+
 
     provider = gtk_css_provider_new();
     gtk_css_provider_load_from_data(
@@ -45,7 +61,7 @@ activate (GtkApplication *app, gpointer user_data)
         "textview {"
         "  font-size: 15px;"
         "  font-family: serif;"
-        "  color: green;"
+        "  color: white;"
         "}",
         -1
     );
@@ -61,17 +77,6 @@ activate (GtkApplication *app, gpointer user_data)
 
     gtk_text_view_set_left_margin(GTK_TEXT_VIEW(view), 30);
 
-    tag = gtk_text_buffer_create_tag(
-        buffer,
-        "blue_foreground",
-        "foreground", "blue",
-        NULL
-    );
-
-    gtk_text_buffer_get_iter_at_offset(buffer, &start, 7);
-    gtk_text_buffer_get_iter_at_offset(buffer, &end, 12);
-    gtk_text_buffer_apply_tag(buffer, tag, &start, &end);
-
     scrolled = gtk_scrolled_window_new();
     gtk_widget_set_hexpand(scrolled, TRUE);
     gtk_widget_set_vexpand(scrolled, TRUE);
@@ -79,7 +84,19 @@ activate (GtkApplication *app, gpointer user_data)
 
     gtk_box_append(GTK_BOX(box), scrolled);
 
+    GtkEventController* controller= gtk_event_controller_key_new();
+    g_signal_connect_object(controller, "key-pressed", G_CALLBACK(event_key_pressed_cb), view, G_CONNECT_SWAPPED);
+    gtk_widget_add_controller(window, controller);
+
     gtk_window_present(GTK_WINDOW(window));
+}
+
+void goto_line() {
+    GtkTextIter iter;
+    gtk_text_buffer_get_iter_at_line(buffer, &iter, 85);
+    GtkTextMark* mark= gtk_text_buffer_create_mark(buffer, "main", &iter, true);
+
+    gtk_text_view_scroll_to_mark(GTK_TEXT_VIEW(view), mark, 0, true, 0, 0);
 }
 
 
