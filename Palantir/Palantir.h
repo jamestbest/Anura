d7a1c0211524a54581140b9573954d99680068e9
james <james2004.coward@gmail.com>
2026-03-13 02:35:07 +0000

    Added more to the GUI; loading of files and line numbers

--- Palantir/Palantir.h
+++ Palantir/Palantir.h
@@ -2,7 +2,14 @@
 // Created by James Coward on 1/29/26.
 //
 
-#ifndef ANURA_PALANTIR_H
-#define ANURA_PALANTIR_H
+#ifndef ANURA_PALANTIR_GUI_H
+#define ANURA_PALANTIR_GUI_H
 
-#endif //ANURA_PALANTIR_H
+#include <gtk/gtk.h>
+
+void* gui_thread_create(void* data);
+gboolean guiup_main_file(gpointer main_filepath);
+gboolean terminal_log(gpointer data);
+gboolean terminal_err(gpointer data);
+
+#endif //ANURA_PALANTIR_GUI_H
