d05f17622e387da39dc28f5be0cd274b5f1d08ad
james <james2004.coward@gmail.com>
2026-03-13 02:35:51 +0000

    Added labelled register printing to TUI

--- Palantir.h
+++ Palantir.h
@@ -5,7 +5,11 @@
 #ifndef ANURA_PALANTIR_H
 #define ANURA_PALANTIR_H
 
+#include "Target.h"
+
 int tui_setup();
 int tui_loop();
+
+void display_labelled_regs(LabelledRegs lregs);
 
 #endif //ANURA_PALANTIR_H
