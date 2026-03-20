e1489f6cd31affeec90e839eceede9a8666a1685
james <james2004.coward@gmail.com>
2026-03-20 03:08:49 +0000

    Changed frame info to not use global matrix but to create one

--- Saruman/FrameInfoInternal.h
+++ Saruman/FrameInfoInternal.h
@@ -14,7 +14,6 @@
 #define DW_AUG_OPTION_PERSONAILTY_ROUTINE_CHR 'P'
 #define DW_AUG_OPTION_ADDRESS_POINTER_CHR 'R'
 
-
 typedef struct CIEAndOffset {
     uint64_t offset;
     CIE_Entry* entry;
@@ -31,8 +30,6 @@ static int read_fde_entry(
 static int read_cie_entry(uint8_t** start, MODE mode, uint8_t* header_end, CIE_Entry* entry);
 
 static Instruction decode_op(uint8_t** start, const CIE_Entry* cie);
-static void execute_op(Instruction instr, const CIE_Entry* cie);
-static void decode_and_execute_op(uint8_t** start, const CIE_Entry* cie);
 
 static int u64_cmp(uint64_t a, uint64_t b) {
     if (a < b) return -1;
