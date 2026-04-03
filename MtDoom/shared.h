371cb8b80f4e354d79414c3e71c3dd3273634ac7
james <james2004.coward@gmail.com>
2026-04-03 02:46:44 +0100

    Added shared file for output and generator

---
+++ MtDoom/shared.h
@@ -0,0 +1,14 @@
+//
+// Created by jamestbest on 3/30/26.
+//
+
+#ifndef ANURA_SHARED_H
+#define ANURA_SHARED_H
+
+typedef enum ENDIANNESS {
+    ENDIAN_LITTLE,
+    ENDIAN_BIG
+} ENDIANNESS;
+extern ENDIANNESS endianness;
+
+#endif //ANURA_SHARED_H
